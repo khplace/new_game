5d7103b6b6a4b0754b1dd4b7cfc4d29b8c9eb8fb
yuna <cyunanne@gmail.com>
2023-03-04 16:01:24 +0900

    메인 뷰 줄 간격 수정

--- project01_KHPlace/src/view/MainMenuView.java
+++ project01_KHPlace/src/view/MainMenuView.java
@@ -23,18 +23,18 @@ public class MainMenuView {
         while(true) {
             try {
                 Service.clearScreen(); // 화면 초기화
-                System.out.println("· ------------------- · ◈ · ------------------- ·");
-                System.out.printf("  안녕하세요~! %s카페 %s사장님 %d일차 장사 준비를 해주세요! \n",owner.getCeo(),owner.getName(),owner.getDay());
-                System.out.printf("  현재 남은 잔고는 %dkh 입니다.\n",owner.getMoney());
-                System.out.printf("  남은 대출금 : %dkh\n",owner.getDept());    // 현 잔여 출액 입력
-                System.out.printf("  다음 턴에 내야하는 이자 : %dkh\n",Service.TodayDept());
-                System.out.println("· ------------------- · ◈ · ------------------- ·");
+                System.out.println("· ------------------- · ◈ · ------------------- ·\n");
+                System.out.printf("  안녕하세요~! %s 카페 %s 사장님 %d 일차 장사 준비를 해주세요! \n",owner.getCeo(),owner.getName(),owner.getDay());
+                System.out.printf("  현재 남은 잔고는 %d kh 입니다.\n",owner.getMoney());
+                System.out.printf("  남은 대출금 : %d kh\n",owner.getDept());    // 현 잔여 출액 입력
+                System.out.printf("  다음 턴에 내야하는 이자 : %d kh\n",Service.TodayDept());
+                System.out.println("\n· ------------------- · ◈ · ------------------- ·\n");
                 System.out.println("  1. 재고 확인");
                 System.out.println("  2. 물건 구매");
                 System.out.println("  3. 장사 시작");
                 System.out.println("  4. 중도 상환");
                 System.out.println("  0. 프로그램 종료 ");
-                System.out.println("· ------------------- · ◈ · ------------------- ·");
+                System.out.println("\n· ------------------- · ◈ · ------------------- ·\n");
 
                 System.out.print("  무엇을 하시겠습니까? >> ");
                 input = Integer.parseInt(sc.nextLine());
