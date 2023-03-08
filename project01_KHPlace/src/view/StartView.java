ae4e6739b8349fb478bbf6aa15a581678cd53d9b
yuna <cyunanne@gmail.com>
2023-03-08 21:48:02 +0900

    게임 초기화면에서 '이어하기' 선택지 추가

--- project01_KHPlace/src/view/StartView.java
+++ project01_KHPlace/src/view/StartView.java
@@ -10,6 +10,7 @@ public class StartView {
     
     private GameInitView gameInitView = new GameInitView(); // 1. 게임 시작 : 게임 초기 정보 입력(플레이어 정보)
     private CreatersView creatersView = new CreatersView(); // 2. 만든 사람
+    private SaveAndLoadView saveAndLoadView = new SaveAndLoadView(); // 3. 이어 하기
 
     public void displayInitialScreenView() {
         int input = 0;
@@ -20,7 +21,8 @@ public class StartView {
             System.out.println("               · K H   P L A C E ·\n");
             System.out.println("                  1. 게임 시작                  ");
             System.out.println("                  2. 만든 사람                  ");
-            System.out.println("                  3. 프로그램 종료                 ");
+            System.out.println("                  3. 이어 하기                  ");
+            System.out.println("                  4. 프로그램 종료                 ");
             System.out.println("\n· ------------------- · ◈ · ------------------- ·");
 
             try {
@@ -28,10 +30,11 @@ public class StartView {
                 input = Integer.parseInt(sc.nextLine());
 
                 switch (input) {
-                    case 1: gameInitView.startNewGame(); break; // 게임 초기 정보 입력(플레이어 정보)
-                    case 2: creatersView.displayCreatersView(); break; // 만든이 정보 출력
-                    case 3: Service.exit(0); return; // 프로그램 종료
-                    default: WrongInputView.wrongInput(); // 잘못된 입력 시 경고문구 출력
+                    case 1: gameInitView.startNewGame(); break;         // 게임 초기 정보 입력(플레이어 정보)
+                    case 2: creatersView.displayCreatersView(); break;  // 만든이 정보 출력
+                    case 3: saveAndLoadView.displayLoadMenu(); break;   // 저장된 플레이 기록 불러 오기
+                    case 4: Service.exit(0); return;              // 프로그램 종료
+                    default: WrongInputView.wrongInput();               // 잘못된 입력 시 경고문구 출력
                 }
             } catch (NumberFormatException e) { // 숫자 입력이 아닌 경우
                 new WrongInputView().wrongInput();
